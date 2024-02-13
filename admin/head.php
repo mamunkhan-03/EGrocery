fba8902cabf0b2e623157c7fc940cd6bf16d15ac
Md Mamun Hossain <99616480+mamunkhan-03@users.noreply.github.com>
2024-02-14 01:39:54 +0600

    Add files via upload

---
+++ admin/head.php
@@ -0,0 +1,20 @@
+<head>
+<title>Admin</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+ <!-- Bootstrap Core CSS -->
+<link href="css/bootstrap.min.css" rel='stylesheet' type='text/css' />
+	<!-- Custom CSS -->
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+	<!-- Graph CSS -->
+<link href="css/font-awesome.css" rel="stylesheet"> 
+	<!-- jQuery -->
+<link href='//fonts.googleapis.com/css?family=Roboto:700,500,300,100italic,100,400' rel='stylesheet' type='text/css'>
+	<!-- lined-icons -->
+<link rel="stylesheet" href="css/icon-font.min.css" type='text/css' />
+	<!-- /js -->
+<script src="js/jquery-1.10.2.min.js"></script>
+<!-- //js-->
+</head>
